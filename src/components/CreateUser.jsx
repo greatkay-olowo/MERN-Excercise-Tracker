35c930a2d5879a7d9e38e5c5059c411e2bb083a5
Kayode Alao <oluwakayode.alao@gmail.com>
2019-12-08 17:33:04 +0100

    complete project

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,8 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const CreateUser = () => {
-  return <div>Create User</div>;
-};
+class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+    };
+  }
 
+  onChangeUsername = e => {
+    this.setState({ username: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const user = {
+      username: this.state.username,
+    };
+
+    axios
+      .post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      username: '',
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h3 className="mt-4 mb-2">Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 export default CreateUser;
--- src/components/ExcerciseList.jsx
+++ src/components/ExcerciseList.jsx
@@ -1,8 +1,86 @@
-import React from 'react';
+/* eslint-disable no-unused-expressions */
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const ExcerciseList = () => {
-  return <div>Excercise List</div>;
-};
+const Excercise = props => (
+  <tr>
+    <td>{props.excercise.username}</td>
+    <td>{props.excercise.description}</td>
+    <td>{props.excercise.duration}</td>
+    <td>{props.excercise.date.substring(0, 10)}</td>
+    <td>
+      <Link to={`/edit/${props.excercise._id}`}>edit</Link> |
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExcercise(props.excercise._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
+
+class ExcerciseList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { excercise: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/excercises/')
+      .then(res => {
+        this.setState({ excercise: res.data });
+        console.log(this.state.excercise);
+      })
+      .catch(err => console.error('Error: ' + err));
+  }
+
+  deleteExcercise = id => {
+    axios
+      .delete(`http://localhost:5000/excercises/${id}`)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      excercise: this.state.excercise.filter(e => e._id !== id),
+    });
+  };
+
+  excerciseList = () => {
+    return this.state.excercise.map(currentexcercise => {
+      return (
+        <Excercise
+          excercise={currentexcercise}
+          deleteExcercise={this.deleteExcercise}
+          key={currentexcercise._id}
+        />
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3 className="mt-4 mb-2">Logged Excercise</h3>
+        <table className="table table-bordered table-hover">
+          <thead className="thead-dark">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>{this.excerciseList()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default ExcerciseList;
